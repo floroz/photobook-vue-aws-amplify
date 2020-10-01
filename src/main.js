0967c43cbf217949687480e9b28dd21603670524
Daniele <tortora87@gmail.com>
2020-10-01 14:20:33 +0100

    fix issue with old vue being used in ui-vue aws

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import Amplify from "aws-amplify";
+import "@aws-amplify/ui-vue";
+import aws_exports from "./aws-exports";
 
-createApp(App).mount('#app')
+Amplify.configure(aws_exports);
+
+const app = createApp(App);
+
+app.config.isCustomElement = (tag) => /amplify-\w*/.match(tag);
+
+app.mount("#app");
