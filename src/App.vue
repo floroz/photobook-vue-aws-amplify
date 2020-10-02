6450fac651564dd6f15e11a9b224a9b2ddcb4e7f
Daniele <tortora87@gmail.com>
2020-10-02 17:08:00 +0100

    add sass and fix polyfill

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
 
 <script>
 import store from "@/store";
+import Navbar from "@/components/Nav.vue";
 export default {
   name: "App",
   store,
   components: {
-    // LoginPage,
-    // AlbumDetailsPage,
-    // AlbumsPage,
-    // SignUp,
-    // Home,
-    // About,
+    Navbar,
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import Amplify from "aws-amplify";
-import "@aws-amplify/ui-vue";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@aws-amplify/ui-components/loader";
 import aws_exports from "./aws-exports";
 import store from "./store";
 import router from "./router";
+/**
+ * I am doing the polyfilling here locally as amplify-ui is not updated for vue 3
+ */
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 Amplify.configure(aws_exports);
 
